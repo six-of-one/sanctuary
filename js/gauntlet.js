a96e45a9d1231f3b3cfc54b85db789eafc240f10
nobody <katty@strem.localdomain>
2020-09-21 19:08:38 -0400

    commentary - if we want a new theif where the last one was shot

--- js/gauntlet.js
+++ js/gauntlet.js
@@ -925,6 +925,7 @@ Gauntlet = function() {
 //document.title = "THFC:  "+thcell.tx+" : "+thcell.ty;
 				Mastermap.load_cell(thcell.tx, thcell.ty, theif_ad,Mastermap);
 				Mastercell.ptr.theif = 0;	// spawned -- NOT generated or placed
+// could start a new thief where the old one was killed - if local thieftrack is saved
 				Mastercell.ptr.thieftrack = 0;	// spawned -- NOT generated or placed
 				Musicth.play(Musicth.sounds.hitheif);
 		}
