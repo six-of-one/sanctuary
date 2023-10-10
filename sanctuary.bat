2e9b13327bca4d540ec4569103dfefd533fc58b8
nobody <psalm@Leviticus>
2023-10-09 23:39:47 -0400

    smol mod

--- sanctuary.bat
+++ sanctuary.bat
@@ -1,5 +1,7 @@
-# command line, sendto, file clicker script
-# silly lock to chrome location
-# if you dont run chrome / chromium - click on the index.html and open it / drag it to web-browser of choice
+:: command line, sendto, file clicker script
+:: silly lock to chrome location
+:: if you dont run chrome / chromium - click on the index.html and open it / drag it to web-browser of choice
 
-"C:\Program Files\chrome-win\chrome.exe" --allow-file-access-from-files file:///%cd%/index.html
+"C:\Program Files (x86)\chrome-win\chrome.exe" --allow-file-access-from-files file:///%cd%\index.html
+::@echo off
+:: set /p input="hit <ENTER>"
