72aadabc4c1d116b54253c9b5685174018b7adc7
six-of-one <quakec@cataboligne.org>
2023-10-20 12:37:35 -0400

    commentary

--- js/gauntlet.js
+++ js/gauntlet.js
@@ -53,6 +53,7 @@ Gauntlet = function() {
 	scoredex = 0,
 	HSCORE = [ 0, "Names", "character" ],
 // g1 custom walls diff from main wall mapped on EXLOB (special handle)
+// invisible wall & shotable invisible are in FAKES, item 15
 			G1WALL = [	0, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 26	],
 
 // handle death potion bomb rotating score - 
@@ -1553,6 +1554,7 @@ Gauntlet = function() {
 			 }
 		 }
       else if (entity.exit)
+// FIXT - make less sensitive for exit maze
         player.exit(entity);
     },
 
