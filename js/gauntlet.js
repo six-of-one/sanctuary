a74f5b14a8ac8431832f2307d398b19b897f4010
six-of-one <quakec@cataboligne.org>
2023-10-26 16:16:18 -0400

    adding unpiny logic .9 - lower left

--- js/gauntlet.js
+++ js/gauntlet.js
@@ -3982,6 +3982,13 @@ var txsv = ":";
 			rh = 0 - this.y;
 			m = Game.Math.overlap(x, y, w, h, rx, ry, rw, rh);
 		}
+		if (txo && tyo) {
+			rx = 0;
+			rw = (this.x + this.w) - Mastermap.w;
+			ry = 0;
+			rh = (this.y + this.h) - Mastermap.h;
+			m = Game.Math.overlap(x, y, w, h, rx, ry, rw, rh);
+		}
 
 		if (k != false || j != false || m != false) return(false);		// return is false if overlap happens
 
