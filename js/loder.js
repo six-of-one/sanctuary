3480a225220e4a51430d018caa46f2d8cdd1c00f
six-of-one <quakec@cataboligne.org>
2023-12-19 20:57:04 -0500

    new loader file

---
+++ js/loder.js
@@ -0,0 +1,13 @@
+
+  //===========================================================================
+  // LODER  base file
+  //===========================================================================
+
+// append all updates and pre-loads...
+
+// restore highscores - edit the restore back out so new scores can be saved
+
+// replay random level saves - remove for fresh level contents
+
+// customize other features here
+
